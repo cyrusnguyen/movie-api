cb7b19bac89781c4a29d05965645d1e7259eca8f
Cyrus <hoangminh0268@Gmail.com>
2023-05-31 15:11:10 +1000

    Complete data and search enpoints

--- routes/movies.js
+++ routes/movies.js
@@ -3,24 +3,39 @@ var router = express.Router();
 const authorization = require("../middleware/authorization");
 
 router.get('/search', async function (req, res, next) {
-  const movies = [];
+
   var page = parseInt(req.query.page) || 1;
-  var per_page = parseInt(req.query.per_page) || 10;
+  var per_page = parseInt(req.query.per_page) || 100;
 
   //check if there are existed invalid params
-  if (!page || !per_page) {
+  if (!isNaN(req.query.year) && isNaN(parseInt(req.query.year))) {
     res.status(400).json({
       error: true,
-      message: `Invalid query parameters`,
+      message: `Invalid year format. Format must be yyyy.`,
     });
   } else {
     var offset = (page - 1) * per_page;
-    var count = await req.db.from('basics').count('* as total').first();
+    var count = await req.db('basics')
+    .where("primaryTitle", "like", `%${req.query.title || ''}%`)
+    .where("year", "like", `%${req.query.year || ''}%`).count("* as total").first();
 
-    req.db.from('basics').select("*").limit(per_page).offset(offset)
+    req.db.from('basics').select("primaryTitle","year","tconst","imdbRating","rottenTomatoesRating","metacriticRating","rated")
+    .where("primaryTitle", "like", `%${req.query.title || ''}%`)
+    .where("year", "like", `%${req.query.year || ''}%`)
+    .limit(per_page).offset(offset)
       .then((rows) => {
-        rows.map((data) => movies.push(data.country));
-        res.status(200).json({ "error": false, "message": "Success", "data": rows, "pagination": {
+        const data = rows.map((row) => {
+          return {
+            "title": row.primaryTitle,
+            "year": parseInt(row.year) || null,
+            "imdbID": row.tconst,
+            "imdbRating": parseInt(row.imdbRating) || null,
+            "rottenTomatoesRating": parseInt(row.rottenTomatoesRating) || null,
+            "metacriticRating": parseInt(row.metacriticRating) || null,
+            "classification": row.rated,
+          }
+        });
+        res.status(200).json({ "data": data, "pagination": {
           total: count.total,
           lastPage: Math.ceil(count.total / per_page),
           perPage: per_page,
@@ -42,43 +57,68 @@ router.get('/search', async function (req, res, next) {
 
 
 router.get("/data/:id", function (req, res, next) {
-  if(!id){
+  if (Object.keys(req.query).length > 0) {
     res.status(400).json({
       error: true,
-      message: `Invalid query parameters: ${id}. Query parameters are not permitted`,
+      message: `Invalid query parameters: ${Object.keys(req.query).join(",")}. Query parameters are not permitted`,
     });
   }
-  req.db.from('basics').select('*').where('id', '=', req.params.id)
-    .then((rows) => {
-      res.json({ "error": false, "message": "Success", "data": rows })
+  req.db.from('basics as b')
+  .join('principals as p', 'b.tconst', 'p.tconst')
+  .join('ratings as r', 'b.tconst', 'r.tconst')
+  .select({
+    title: 'b.primaryTitle',
+    year: 'b.year',
+    runtime: 'b.runtimeMinutes',
+    genres: 'b.genres',
+    country: 'b.country',
+    boxoffice: 'b.boxoffice',
+    poster: 'b.poster',
+    plot:'b.plot'
+  }, {
+    id: 'p.nconst', 
+    category:'p.category', 
+    name:'p.name', 
+    characters: 'p.characters'
+  }, 'r.source', 'r.value')
+  .where('b.tconst', '=', req.params.id).groupBy('b.tconst','p.nconst', 'p.name', 'p.category', 'p.characters', 'r.source', 'r.value')
+  .then((rows) => {const result = rows.reduce((acc, row) => {
+    const { title, year, runtime, country, boxoffice, poster, plot, id, category, name, source, value} = row;
+    const genres = row.genres.split(',') ;
+    const characters = row.characters.trim() === "" ? "" : JSON.parse(row.characters);
+
+    if (!acc.principals.some((p) => p.id === id)) {
+    acc.principals.push({ id, category, name, characters });
+    }
+    if (!acc.ratings.some((r) => r.source === source)) {
+    acc.ratings.push({ source, value });
+    }
+   
+    
+    acc = {
+      title,
+      year,
+      runtime,
+      genres,
+      country,
+      principals: acc.principals,
+      ratings: acc.ratings,
+      boxoffice,
+      poster,
+      plot
+    };
+   
+    return acc;
+    }, {principals:[], ratings:[]});
+  
+      res.status(200).json( result )
+      
     })
     .catch((err) => {
       console.log(err);
-      res.json({ "error": true, "message": "Error executing MySQL query" })
+      res.status(500).json({ "error": true, "message": "Database error"})
     })
 });
 
-// router.post('/api/update', authorization, (req, res) => {
-//   if (!req.body.City || !req.body.CountryCode || !req.body.Pop) {
-//     res.status(400).json({ message: `Error updating population` });
-//     console.log(`Error on request body:`, JSON.stringify(req.body));
-
-//   } else {
-//     const filter = {
-//       "Name": req.body.City,
-//       "CountryCode": req.body.CountryCode
-//     };
-//     const pop = {
-//       "Population": req.body.Pop
-//     };
-//     req.db('city').where(filter).update(pop)
-//       .then(_ => {
-//         res.status(201).json({ message: `Successful update ${req.body.City}` });
-//         console.log(`successful population update:`, JSON.stringify(filter));
-//       }).catch(error => {
-//         res.status(500).json({ message: 'Database error - not updated' });
-//       });
-//   }
-// });
 
 module.exports = router;
