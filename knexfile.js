5dadab81df6a168585c3ca91da55d00897807bb0
cyrusnguyen <hoangminh0268@gmail.com>
2024-02-11 21:17:49 +1000

    Fixed Db issues

--- knexfile.js
+++ knexfile.js
@@ -1,9 +1,4 @@
 module.exports = {
     client: 'mysql2',
-    connection: {
-        host: '127.0.0.1',
-        database: process.env.DB_NAME,
-        user: process.env.DB_USERNAME,
-        password: process.env.DB_PASSWORD
-    }
+    connection: process.env.DATABASE_URL
 }
